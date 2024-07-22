7e88ac5961d6926bd98593b86d77574cb2a81dce
Bhagavath Nimmagadda <bhagavath@Bhagavaths-MacBook-Air.local>
2024-07-23 01:32:18 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+        source={{
+          uri: "http://103.163.69.241/api/v1/tiles/styles/streets/?api_key=05a9e976c1764b2b92fe3ae8cfdd0e37&vector#10.75/18.9312/72.8209/0/21",
+        }}
+        style={{ flex: 1 }}
+      />
     </View>
   );
 }
@@ -13,8 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
